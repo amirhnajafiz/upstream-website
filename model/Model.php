28aec171685e2a14c3587e368e5ebda75603dbf2
9831065 <najafizadeh21@gmail.com>
2021-08-10 20:01:07 +0430

    Base model added

--- model/Model.php
+++ model/Model.php
@@ -2,9 +2,26 @@
 
 namespace mvc\model;
 
+/**
+ * Model class is the superclass of all our models,
+ * which has a connector and table_name. 
+ * 
+ */
 abstract class Model 
 {
-    // The base model
+    // Private fields
+    protected PDO $connector;
+    protected string $table_name;
+
+    public abstract function Do();
+
+    public abstract function insert();
+
+    public abstract function update();
+
+    public abstract function delete();
+
+    public abstract function select();
 }
 
 ?>
