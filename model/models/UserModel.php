73fe585db15925dfc23716dd53bc8475112885e2
9831065 <najafizadeh21@gmail.com>
2021-08-12 09:55:03 +0430

    User models in model updated

--- model/models/UserModel.php
+++ model/models/UserModel.php
@@ -9,6 +9,31 @@ class UserModel
     private int $status;
     private int $isAdmin;
     private int $canConfirm;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function isAdmin()
+    {
+        return $this->isAdmin;
+    }
+
+    public function canConfirm()
+    {
+        return $this->canConfirm;
+    }
 }
 
 ?>
