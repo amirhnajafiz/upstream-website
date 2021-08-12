fb4d2101c41d30f252abef448df0817add125377
9831065 <najafizadeh21@gmail.com>
2021-08-12 09:58:37 +0430

    File models in model updated

--- model/models/FileModel.php
+++ model/models/FileModel.php
@@ -15,6 +15,61 @@ class FileModel
     private int $size;
     private date $expireDate;
     private int $valid;
+
+    public function getID()
+    {
+        return $this->ID;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    public function getUploader()
+    {
+        return $this->uploader;
+    }
+
+    public function isActive()
+    {
+        return $this->isActive;
+    }
+
+    public function uploadDate()
+    {
+        return $this->uploadDate;
+    }
+
+    public function getDownloads()
+    {
+        return $this->downloads;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function expireDate()
+    {
+        return $this->expireDate;
+    }
+
+    public function isValid()
+    {
+        return $this->valid;
+    }
 }
 
 ?>
