f76488e6ded910bf7699ea60b33dd52ffc7dd1c9
9831065 <najafizadeh21@gmail.com>
2021-08-14 10:18:51 +0430

    Update model of database

--- model/Model.php
+++ model/Model.php
@@ -2,6 +2,8 @@
 
 namespace mvc\model;
 
+use mvc\database\Database;
+
 /**
  * Model class is the superclass of all our models,
  * which has a connector and table_name. 
@@ -12,10 +14,30 @@ abstract class Model
     // Private fields
     protected PDO $connector;
 
-    public function __construct() {
-
+    public function __construct() 
+    {
+        $this->connector = Database::getInstance()->getPDO();
     }
-    
+
+    public function select($table_name, array $where = [])
+    {
+        // TODO: Select query
+    }
+
+    public function insert($table_name, array $data = [])
+    {
+        // TODO: Insert query
+    }
+
+    public function update($table_name, array $data = [])
+    {
+        // TODO: Update query
+    }
+
+    public function delete($table_name, array $where = [])
+    {
+        // TODO: Delete query
+    }
 }
 
 ?>
